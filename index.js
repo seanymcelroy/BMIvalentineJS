9767275a2d678065c49046bdd698a2808c155104
Sean McElroy <seanpetermcelroy@gmail.com>
2024-02-07 22:26:04 +0000

    Try again added if swipe left action is triggered.

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ let startY;
 let initialX;
 
 let isDragging = false;
+let isAnimationInProgress = false;
 
 const card = document.getElementById('card');
 const bmi = document.getElementById('bmi');
 const likeBox = document.getElementById('likeBox');
 const uglyBox = document.getElementById('uglyBox');
+const wrongAnswerBox = document.getElementById('wrongAnswer');
 
 card.addEventListener('touchstart', handleTouchStart);
 card.addEventListener('touchmove', handleTouchMove);
 card.addEventListener('touchend', handleTouchEnd);
 
 function handleTouchStart(event) {
+  // Check if animations are in progress; if so, exit the function
+    if (isAnimationInProgress) {
+        return;
+    }
     startX = event.touches[0].clientX;
     initialX = card.offsetLeft;
     isDragging = true;
@@ -31,9 +37,11 @@ function handleTouchMove(event) {
           // card.style.backgroundColor='red';
           likeBox.style.display='none';
           uglyBox.style.display='inline-block';
+          card.style.boxShadow='0 0 20px #F74A66';
         }else{
           uglyBox.style.display='none';
           likeBox.style.display='inline-block';
+          card.style.boxShadow='0 0 20px #63DE9B';
           // card.style.backgroundColor='green';
         }
         let rotationAngle = offsetX / 10;
@@ -58,16 +66,35 @@ function match() {
   card.style.left = `0%`;
   card.style.top = `0%`;
   card.style.transform = `rotate(0deg)`;
+
+  // Set the animation flag to true to indicate animations are in progress
+  isAnimationInProgress = true;
+
+  // Create an image element for the match overlay
+  const matchOverlay = document.createElement('img');
+  matchOverlay.src = 'itsAMatch.png';
+  matchOverlay.style.position = 'absolute';
+  matchOverlay.style.top = '50%';
+  matchOverlay.style.left = '50%';
+  matchOverlay.style.transform = 'translate(-50%, -50%)';
+  matchOverlay.style.zIndex = '999'; // Ensure it's on top of other elements
+  matchOverlay.style.transition = 'opacity 0.5';
+
+  // Append the match overlay to the card element
+  card.appendChild(matchOverlay);
 }
 
 function left() {    
-  isDragging = false;
+    isDragging = false;
+    isAnimationInProgress=true;
     card.style.transition = 'left 0.5s, background-color 0.5s, transform 0.5s'; // Apply transitions
     card.style.left = 50 + '%';
-    card.style.backgroundColor = 'pink';
     card.style.transform = `translate(-50%, -50%) rotate(0deg)`;
-
+    
     setTimeout(() => {
+        uglyBox.style.display='none';
+        wrongAnswerBox.style.display='inline-block';
+        card.style.boxShadow='0 0 20px #4ab2f7';
         card.style.transition = 'left 0.5s, background-color 0.5s, transform 0.1s';
         card.style.transform = `translate(-50%, -50%) rotate(2deg)`;  
     }, 500); // Duration of transition in milliseconds
@@ -84,24 +111,23 @@ function left() {
       card.style.transform = `translate(-50%, -50%) rotate(0deg)`;  
       card.style.backgroundColor = 'grey';
     }, 900); // Duration of transition in milliseconds
-    // setTimeout(() => {
-    //     card.style.transform = `translate(-60%, -50%) rotate(-5deg)`;  
-    // }, 400); // Duration of transition in milliseconds
-    // setTimeout(() => {
-    //     card.style.transform = `translate(-50%, -50%) rotate(0deg)`;  
-    // }, 600); // Duration of transition in milliseconds
-
-
 
     // Reset transitions after transition ends
     setTimeout(() => {
         card.style.transition = 'none';
+        card.style.boxShadow = 'none';
+        isAnimationInProgress=false;
+        wrongAnswerBox.style.display = 'none';
     }, 1300); // Duration of transition in milliseconds
 }
 
 
 
 function handleTouchEnd(event) {
+  // Check if animations are in progress; if so, exit the function
+    if (isAnimationInProgress) {
+      return;
+    }
     if (!isDragging) {
       return; // Exit the function if not dragging
     } 
@@ -110,6 +136,7 @@ function handleTouchEnd(event) {
     card.style.left = 50 + '%';
     card.style.backgroundColor = 'grey';
     card.style.transform = `translate(-50%, -50%) rotate(0deg)`;
+    card.style.boxShadow = 'none';
     likeBox.style.display='none';
     uglyBox.style.display='none';
     
